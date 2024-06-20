d406f35ccd82b31400ebb1beae36eeafdd82c81f
Noelia <abascalcabreran@gmail.com>
2024-06-20 09:29:23 +0200

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
+import getDataApi from './services/api.js';
 import Header from './components/Header';
 import List from './components/List';
 //import moviesExample from './mocks/moviesExample.json';
@@ -9,9 +10,13 @@ React;
 
 function App(): JSX.Element {
   //estados
-  const [moviesAppi, setMoviesAppi] = useState<SearchMovies>([]);
+  const [movies, setMovies] = useState<SearchMovies>([]);
   const [query, setQuery] = useState<string>('');
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  //constante a modo de useRef para que guarde como referencia la query y
+  //no nos permita hacer la misma búsqueda 2 veces seguidas
+  const previousQuery = useRef<string>(query);
 
   //ejemplo de datos que devolverá el fetch para ir pintando unas películas o un error
   //const movies: SearchMovies = moviesExample.Search;
@@ -19,14 +24,13 @@ function App(): JSX.Element {
 
   //función fetch a la api
   useEffect(() => {
-    fetch(`http://www.omdbapi.com/?apikey=56aa5655&s=${query}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setMoviesAppi(data.Search);
-      })
-      .catch((error) => {
-        console.error(console.log(error));
-      });
+    //si la query es igual que la query anterior, no hace el fetch de getDataApi
+    if (query === previousQuery.current) {
+      return;
+    }
+    getDataApi(query).then((movies: SearchMovies) => {
+      setMovies(movies);
+    });
   }, [query]);
 
   //función para setear el estado con el valor del input
@@ -48,7 +52,7 @@ function App(): JSX.Element {
         onError={handleError}
       />
       <main className="main">
-        <List movies={moviesAppi} />
+        <List movies={movies} />
       </main>
     </div>
   );
--- src/services/Api.js
+++ src/services/Api.js
@@ -1,6 +1,20 @@
-// export function getDataApi() {
-//   fetch('http://www.omdbapi.com/?apikey=56aa5655&s');{
-//     .then(res=response);
-//     .then(const data=JSON.parse(res))
-//   }
-// }
+const API_KEY = '56aa5655';
+
+function getDataApi(query) {
+  if (query === '') {
+    return null; // no haríamos el fetch
+  } else {
+    return fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesData = data.Search;
+        return moviesData;
+      })
+      .catch((error) => {
+        console.error('Error fetching data: ', error);
+        throw error;
+      });
+  }
+}
+
+export default getDataApi;
